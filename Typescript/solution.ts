9bd07171f7fcefa05471e7e395e41708e207c579
Andriy Chernikov <chernikov@gmail.com>
2019-11-23 10:35:58 +0200

    [javascript] Find The Unique https://www.codewars.com/kata/5862e0db4f7ab47bed0000e5

--- Typescript/solution.ts
+++ Typescript/solution.ts
@@ -1,18 +1,24 @@
-function isSame(a : string, b : string) : number {
-    var astr = a.toLowerCase().split('').sort()
-    .filter((value, index, self) => self.indexOf(value) === index).join('');
-  var bstr = b.toLowerCase().split('').sort()
-    .filter((value, index, self) => self.indexOf(value) === index).join('');
-  if (astr > bstr) {
-  return -1;
-  }
-  if (bstr > astr) {
-    return 1;
-  }
-  return 0;
-  }
+function isSame(a : any, b : any) : number 
+{   
+    let astr = a.toString();
+    let bstr = b.toString();
+    if (astr > bstr) {
+      return -1;
+    }
+    if (bstr > astr) {
+      return 1;
+    }
+    return 0;
+}
 
-export function findUniq(arr: string[]): string {
+export function findUniq(arr) {
   arr = arr.sort(isSame);
   return isSame(arr[0], arr[1]) == 0 ? arr[arr.length - 1] : arr[0];
 }
+
+
+/*
+function findUniq(arr) { 
+return arr.find(i => i === i && arr.indexOf(i) == arr.lastIndexOf(i));
+}
+*/
--- Typescript/tests/solution.test.ts
+++ Typescript/tests/solution.test.ts
@@ -3,14 +3,15 @@ import {findUniq} from '../solution';
 
 describe('findUniq', () => {
   it('should handle sample cases', () => {
-    assert.strictEqual(findUniq(['Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a']), 'BbBb');
-    assert.strictEqual(findUniq(['abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba']), 'foo');
-    assert.strictEqual(findUniq(['silvia', 'vasili', 'victor']), 'victor');
-    assert.strictEqual(findUniq(['Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter']), 'Harry Potter');
-    assert.strictEqual(findUniq(['    ', 'a', ' ']), 'a');
+    assert.strictEqual(findUniq([ 1, 1, 1, 2, 1, 1 ]), 2);
+    assert.strictEqual(findUniq([ 4, 4, 'foo', 4 ]), 'foo');
+    // assert.strictEqual(findUniq(['abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba']), 'foo');
+    // assert.strictEqual(findUniq(['silvia', 'vasili', 'victor']), 'victor');
+    // assert.strictEqual(findUniq(['Tom Marvolo Riddle', 'I am Lord Voldemort', 'Harry Potter']), 'Harry Potter');
+    // assert.strictEqual(findUniq(['    ', 'a', ' ']), 'a');
 
-    assert.strictEqual(findUniq([ '', '', '', 'a', '', '' ]), 'a');
-    assert.strictEqual(findUniq([ '    ', '  ', ' ', 'a', ' ', '' ]), 'a');
-    assert.strictEqual(findUniq([ 'foobar', 'barfo', 'fobara', '   ', 'fobra', 'oooofrab' ]), '   ');
+    // assert.strictEqual(findUniq([ '', '', '', 'a', '', '' ]), 'a');
+    // assert.strictEqual(findUniq([ '    ', '  ', ' ', 'a', ' ', '' ]), 'a');
+    // assert.strictEqual(findUniq([ 'foobar', 'barfo', 'fobara', '   ', 'fobra', 'oooofrab' ]), '   ');
   });
 });
