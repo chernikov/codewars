4b5389044ebb7a5c51c7652b0092297244a90d01
Andriy Chernikov <chernikov@gmail.com>
2019-11-24 16:14:03 +0200

    Total increasing or decreasing numbers up to a power of 10
    https://www.codewars.com/kata/55b195a69a6cc409ba000053/train/csharp

--- C#/CodeWars.Tests/Tests.cs
+++ C#/CodeWars.Tests/Tests.cs
@@ -5,13 +5,42 @@ namespace CodeWars.Tests
 {
     public class Tests
     {
+        private static void Tester(int inp, BigInteger exp)
+        {
+            Assert.AreEqual(exp, TotalIncreasingOrDecreasingNumbers.TotalIncDec(inp), "Should return the total below 10<sup>" + inp + "</sup>");
+        }
 
         [Test]
-        public void Test1()
+        public void BasicTests()
         {
-            
-            //Assert.AreEqual(" example.", Kata.Rot13(" rknzcyr."));
-            Assert.AreEqual("AdZHVWdC)UFKsy'B-gosxnz0SLCveTffnnHoAeeZnGK`_EzlUx", Kata.Rot13("NqMUIJqP)HSXfl'O-tbfkam0FYPirGssaaUbNrrM{TX`_RmyHk"));
+            // Check(100, 200, 47);
+            // Check(201, 300, 41);
+            // Check(301, 400, 37);
+            // Check(401, 500, 35);
+            // Check(501, 600, 35);
+            // Check(601, 700, 37);
+            // Check(701, 800, 41);
+            // Check(801, 900, 47);
+            // Check(901, 1000, 55);
+
+            // Tester(0, BigInteger.Parse("1"));
+            //Tester(1, BigInteger.Parse("10"));
+            //Tester(2, BigInteger.Parse("100"));
+            //Tester(3, BigInteger.Parse("475"));
+           //  Tester(4, BigInteger.Parse("1675"));
+             Tester(287, BigInteger.Parse("1305173325339115385"));
+        }
+
+        [Test]
+        public void TestIncrease() {
+            // //Assert.AreEqual(1, TotalIncreasingOrDecreasingNumbers.TotalIncrease(1, 0));
+            // Assert.AreEqual(BigInteger.Parse("8"), TotalIncreasingOrDecreasingNumbers.TotalIncrease(1, 2));
+            // Assert.AreEqual(BigInteger.Parse("7"), TotalIncreasingOrDecreasingNumbers.TotalIncrease(2, 2));
+            // Assert.AreEqual(BigInteger.Parse("0"), TotalIncreasingOrDecreasingNumbers.TotalIncrease(9, 2));
+            //Assert.AreEqual(BigInteger.Parse("45"), TotalIncreasingOrDecreasingNumbers.TotalDecrease(9, 3));
+            //Assert.AreEqual(BigInteger.Parse("1"), TotalIncreasingOrDecreasingNumbers.TotalDecrease(1, 3));
+            //Assert.AreEqual(BigInteger.Parse("45"), TotalIncreasingOrDecreasingNumbers.TotalIncrease(1, 3));
+            //Assert.AreEqual(BigInteger.Parse("36"), TotalIncreasingOrDecreasingNumbers.TotalIncrease(2, 3));
         }
     }
 }
--- C#/CodeWars/Kata.cs
+++ C#/CodeWars/Kata.cs
@@ -1,30 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace CodeWars
 {
-    public class Kata
+    using System;
+    using System.Linq;
+
+    public class TotalIncreasingOrDecreasingNumbers
     {
-        public static string Rot13(string input)
+        //https://www.codewars.com/kata/55b195a69a6cc409ba000053/train/csharp
+        public static BigInteger TotalIncDec(int x)
         {
-            var output = "";
-            for(int i = 0; i < input.Length; i++) {
-                char current = input[i];
-                
-                var diff = current == char.ToLower(current) ? (current - 'a') : (current - 'A');
-                if (diff >= 26 || diff < 0) {
-                    output += current;
-                } else if (diff >= 13) {
-                    current = (char)(current - 13);
-                    output += current;
-                } else {
-                    current = (char)(current + 13);
-                    output += current;
-                }
+            if (x == 0)
+            {
+                return 1;
             }
-            return output;
+            var inc = new BigInteger[9]; 
+            var dec = new BigInteger[9];
+            BigInteger total = 10;
+            for (int i = 1; i < x; i++)
+            {
+                for (int index = 0; index < inc.Length; index++)
+                {
+                    BigInteger sum = 0;
+                    for (int range = index; range < inc.Length; range++)
+                    {
+                        if (range > index)
+                        {
+                            sum++;
+                        }
+                        sum += inc[range];
+                    }
+                    inc[index] = sum;
+                }
+                for (int index = dec.Length - 1; index >= 0; index--)
+                {
+                    BigInteger sum = 0;
+                    for (int range = index; range >= 0; range--)
+                    {
+                        if (range <= index)
+                        {
+                            sum++;
+                        }
+                        sum += dec[range];
+                    }
+                    dec[index] = sum;
+                }
+                total += Sum(inc) + Sum(dec) + 9;
+            }
+            return total;
+        }
+
+        private static BigInteger Sum(BigInteger[] array)
+        {
+            BigInteger total = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                total += array[i];
+            }
+            return total;
         }
     }
 }
